18ba5642120bf79eb93b2fbda4ce93f212dbaf42
Martin Prucha <prucha.martin@gmail.com>
2018-06-25 08:42:01 +0200

    Lecture 525 completed

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,9 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +35,15 @@ public class CustomerRestController {
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		}
 		
+		return theCustomer;
+	}
+	
+	// add mapping for POST /customer - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		// force the save of new item - set id to 0
+		theCustomer.setId(0);
+		customerService.saveCustomer(theCustomer);
 		return theCustomer;
 	}
 }
